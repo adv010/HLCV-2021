7f8d6d6a984ac32810ef0ba5f13c202344266086
akshay <akmu00001@stud.uni-saarland.de>
2021-06-22 00:54:06 +0200

    Made sync script bidirectional

--- Project/sync_project.sh
+++ Project/sync_project.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-rsync -aP ./ hlcv_team021@conduit.cs.uni-saarland.de:/home/hlcv_team021/hlcv2021/Project
+if [ $1 = "--remote" ]; then
+	echo "Tranferring local data to remote."
+	rsync -aP ./ hlcv_team021@conduit.cs.uni-saarland.de:/home/hlcv_team021/hlcv2021/Project
+elif [ $1 = "--local" ]; then
+	echo "Tranferring remote data to local."
+	rsync -aP hlcv_team021@conduit.cs.uni-saarland.de:/home/hlcv_team021/hlcv2021/Project/ .
+else
+	echo "Provide a valid argument."
+fi
